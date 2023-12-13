6c45b73145572caa9c39369886a6e58be03d0846
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-13 13:50:58 +0100

    create competition model

---
+++ src/app/competition/models/competition.ts
@@ -0,0 +1,10 @@
+export interface Competition{
+  id: number;
+  name: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+  numberOfParticipants: number;
+  location: string;
+  amount: number;
+}
